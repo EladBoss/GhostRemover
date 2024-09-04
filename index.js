5990e8b08361f3845117cbec4ee474c8365c7673
EladBoss <eladcohenyt@gmail.com>
2024-09-03 18:21:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["לקניית גישה פרימיום פתחו טיקט🛒"];
 
 
 let currentIndex = 0;
